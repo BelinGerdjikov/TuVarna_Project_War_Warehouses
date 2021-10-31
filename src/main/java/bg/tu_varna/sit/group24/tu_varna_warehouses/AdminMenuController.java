1f0445e7fbda1e348f175ca7f956499fb077aef2
BelinGerdjikov <belin.gerdjikov19@gmail.com>
2021-10-31 16:29:45 +0200

    #4

--- src/main/java/bg/tu_varna/sit/group24/tu_varna_warehouses/AdminMenuController.java
+++ src/main/java/bg/tu_varna/sit/group24/tu_varna_warehouses/AdminMenuController.java
@@ -1,4 +1,2 @@
-package bg.tu_varna.sit.group24.tu_varna_warehouses;
-
-public class AdminMenuController {
+package bg.tu_varna.sit.group24.tu_varna_warehouses;public class AdminMenuController {
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,15 +1,2 @@
-module bg.tu_varna.sit.group24.tu_varna_warehouses {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.web;
-
-    requires org.controlsfx.controls;
-    requires com.dlsc.formsfx;
-    requires validatorfx;
-    requires org.kordamp.ikonli.javafx;
-    requires org.kordamp.bootstrapfx.core;
-    requires eu.hansolo.tilesfx;
-
-    opens bg.tu_varna.sit.group24.tu_varna_warehouses to javafx.fxml;
-    exports bg.tu_varna.sit.group24.tu_varna_warehouses;
+module $MODULE_NAME$ {
 }
